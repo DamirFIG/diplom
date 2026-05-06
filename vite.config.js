279c5e9aa39ffb9fe30b75b804e0bf72c8acb9b4
DamirFIG <figmafigma10@gmail.com>
2026-05-06 14:43:00 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'public/css/app.css',
+                'public/css/admin.css',
+                'public/js/app.js'
+            ],
             refresh: true,
         }),
     ],
